f1b26dcc983186e1e791b53857319b27bb89e2da
y-kzm <softietennis0212kazuma@gmail.com>
2022-12-07 14:37:16 +0900

    update commands

--- pkg/commands/cmd.go
+++ pkg/commands/cmd.go
@@ -1,29 +1,40 @@
 package commands
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"
+
+	"github.com/y-kzm/enrd-system/cmd/controller/app"
+)
 
 var Commands = []*cli.Command{
 	commandInit,
+	commandTemp,
 	commandConf,
 	commandEstimate,
 }
 
+var commandTemp = &cli.Command{
+	Name:   "template",
+	Usage:  "Generate tinet config template file",
+	Action: app.CmdTemp,
+}
+
 var commandInit = &cli.Command{
 	Name:   "init",
-	Usage:  "Generate tinet config template file",
-	Action: CmdInit,
+	Usage:  "Initialize the database",
+	Action: app.CmdInit,
 }
 
 var commandConf = &cli.Command{
-	Name:   "conf",
+	Name:   "config",
 	Usage:  "configure Node from tinet config file",
-	Action: CmdConf,
+	Action: app.CmdConf,
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:    "config",
 			Aliases: []string{"c"},
 			Usage:   "Specify the Config file.",
-			Value:   "conf.yaml",
+			Value:   "config.yaml",
 		},
 	},
 }
@@ -31,5 +42,13 @@ var commandConf = &cli.Command{
 var commandEstimate = &cli.Command{
 	Name:   "estimate",
 	Usage:  "start to estimate",
-	Action: CmdEstimate,
+	Action: app.CmdEstimate,
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "param",
+			Aliases: []string{"c"},
+			Usage:   "Specify the Parameter file.",
+			Value:   "param.yaml",
+		},
+	},
 }
