921ed793df1202b3fc9745e9ed62c5bcec292a2e
y-kzm <softietennis0212kazuma@gmail.com>
2023-01-01 15:59:45 +0900

    Update scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MODE=${1}
-if [ $# != 1 ]; then
+ETH=${2}
+if [ $# != 2 ]; then
         echo "Usage: ./${PROGRAM} [mode]"
         echo "mode: controller, comptue1, ..., compute4"
         exit 0;
@@ -11,13 +12,13 @@ if [ ${MODE} = "compute1" -o ${MODE} = "com1" ]; then
         ip -6 rule del from fd00:0:172:16::ffff:2/64
         ip -6 rule del from fd00:0:172:16::ffff:3/64
 
-        ip -6 route del fc00:4::/64 encap seg6 mode encap segs fc00:2:: dev enp6s0 table 100 metric 1024 pref medium
-        ip -6 route del fc00:4::/64 encap seg6 mode encap segs fc00:3:: dev enp6s0 table 101 metric 1024 pref medium
-        ip -6 route del fc00:4::/64 encap seg6 mode encap segs fc00:3::,fc00:2:: dev enp6s0 table 102 metric 1024 pref medium
+        ip -6 route del fc00:4::/64 encap seg6 mode encap segs fc00:2:: dev ${ETH} table 100 metric 1024 pref medium
+        ip -6 route del fc00:4::/64 encap seg6 mode encap segs fc00:3:: dev ${ETH} table 101 metric 1024 pref medium
+        ip -6 route del fc00:4::/64 encap seg6 mode encap segs fc00:2::,fc00:3:: dev ${ETH} table 102 metric 1024 pref medium
 
-        ip addr del fd00:0:172:16::ffff:1/64 dev enp6s0
-        ip addr del fd00:0:172:16::ffff:2/64 dev enp6s0
-        ip addr del fd00:0:172:16::ffff:3/64 dev enp6s0
+        ip addr del fd00:0:172:16::ffff:1/64 dev ${ETH}
+        ip addr del fd00:0:172:16::ffff:2/64 dev ${ETH}
+        ip addr del fd00:0:172:16::ffff:3/64 dev ${ETH}
 
         ip addr del fc00:1::/64 dev lo
         ip route del fc00:1::/64
@@ -44,4 +45,4 @@ echo "-------------------------"
 ip -6 route show
 echo "-------------------------"
 ip addr show lo
-ip addr show enp6s0
+ip addr show ${ETH}
